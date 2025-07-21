fd34043887d0449aa1012ee97c9bd835d9c0ce09
EddieSmith <145656018+lordwee@users.noreply.github.com>
2025-07-21 17:42:17 +0200

    i had enough of this shit vs code... i dont want to see the suggestions .. and it's maddening that you cant easily turn them off

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("Hello World");
+const myLibrary = [];
+
+function Book(title, author, pages) { 
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+}
+
+function addBookToLibrary(title, author, pages) { 
+    return myLibrary.push(new Book(title, author, pages))
+}
+addBookToLibrary("Harry Potter", "J.K. Rowling", 355);
+console.log(myLibrary);
+const body = document.querySelector("body")
+const addBook = document.querySelector("button");
+ 
+addBook.addEventListener('click', function () {
+    let form = document.createElement("form");
+    form.setAttribute("id", "add-book");
+    body.appendChild(form); 
+
+    const bookTitleLabel = document.createElement("label")
+    bookTitleLabel.textContent = "Book Title: ";
+    bookTitleLabel.setAttribute("for", "bookTitle");
+    bookTitleLabel.setAttribute("id", "newBook");
+
+    const bookTitle = document.createElement("input");
+    bookTitle.setAttribute("type", "text");
+    bookTitle.setAttribute("id", "bookTitle");
+    bookTitle.setAttribute("name", "booktitle");
+
+    form.appendChild(bookTitleLabel);
+    form.appendChild(bookTitle);
+
+    const bookAuthor = document.createElement(
+})
